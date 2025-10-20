ab71a0abea22edcbcf8ed82d3f53476f8c4f5451
Juliya <iuliia.sharipova@gmail.com>
2025-10-20 18:57:50 +0200

    change

--- hw1/hw1_pack.sh
+++ hw1/hw1_pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -N mpi_bandwidth
-#PBS -l select=1:ncpus=4:mem=2gb
+#PBS -N mpi_bandwidth_pack
+#PBS -l select=1:ncpus=2:mem=2gb
 #PBS -l walltime=0:10:00
 #PBS -q short_cpuQ
 
@@ -8,4 +8,4 @@ cd $PBS_O_WORKDIR
 module load mpich-3.2
 mpicc -O2 -o mpi_bandwidth mpi_bandwidth.c
 
-mpirun.actual -np 2 ./mpi_bandwidth > mpi_results.txt
+mpirun.actual -np 2 ./mpi_bandwidth > mpi_results_pack.txt
