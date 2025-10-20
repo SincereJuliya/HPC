f6a22dacf564bb854e2964b63d732bcfd6a230f8
Juliya <iuliia.sharipova@gmail.com>
2025-10-20 15:55:46 +0200

    updated sh

--- hello_world.cpp
+++ hello_world.cpp
@@ -14,6 +14,6 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     // Print off a hello world message
     printf("Hello world from process rank %d out of %d processors\n", world_rank, world_size);
-
+    // Finalize the MPI environment. No more MPI calls can be made after this
     MPI_Finalize();
 }
