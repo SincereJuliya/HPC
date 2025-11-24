b41b262981c51aa0fa9d2158652cc01627836973
Juliya <iuliia.sharipova@gmail.com>
2025-11-24 14:37:52 +0100

    hw_1 final v
    just some changes in the code + pushed results

--- hw1/plot_bw.py
+++ hw1/plot_bw.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# pack или spread
-filename = "mpi_results_pack.txt"
-data = np.loadtxt(filename)
-
-message_size = data[:,0]
-bandwidth = data[:,2]
+files = ["mpi_results_pack.txt", "mpi_results_spread.txt"]
 
 plt.figure(figsize=(8,5))
-plt.plot(message_size, bandwidth, marker='o')
+
+for f in files:
+    data = np.loadtxt(f)
+    message_size = data[:,0]
+    bandwidth = data[:,2]
+    plt.plot(message_size, bandwidth, marker='o', label=f)
+
 plt.xscale('log')
-plt.xlabel("Msg Size (bytes)")
-plt.ylabel("(MB/s)")
-plt.title(f"MPI Bandwidth vs Message Size ({filename})")
+plt.xlabel("Message size (bytes)")
+plt.ylabel("Bandwidth (MB/s)")
+plt.title("MPI P2P Communication Benchmark")
 plt.grid(True)
+plt.legend()
+plt.savefig("bandwidth_comparison.png")
 plt.show()
